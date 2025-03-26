30d5e1333d156fe4066b67ff93cffda2ba83750f
TJ Saunders <tj@castaglia.org>
2025-03-26 06:24:26 -0700

    Issue #1898: Restore `LDAPDefaultGID` functionality, broken due to regression in change for Issue #1716.

--- contrib/mod_ldap.c
+++ contrib/mod_ldap.c
@@ -944,6 +944,9 @@ static struct passwd *pr_ldap_user_lookup(pool *p, char *filter_template,
               "LDAPDefaultGID Auto in effect but user name is missing");
             return NULL;
           }
+
+        } else {
+          pw->pw_gid = ldap_defaultgid;
         }
         ++i;
 
